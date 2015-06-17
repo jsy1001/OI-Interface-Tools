820d2664e46150d3e2c11bdf691a1feea57be62c
John Young <jsy1001@cam.ac.uk>
2015-06-17 17:24:36 +0100

    Implemented all script commands and unit tests for them.

--- image_oi_input.py
+++ image_oi_input.py
@@ -8,11 +8,120 @@ import astropy.io.fits as fits
 
 from InitImg import InitImg
 
+INIT_IMG_EXTNAME = 'IMAGE-OI INITIAL IMAGE'
 INPUT_PARAM_EXTNAME = 'IMAGE-OI INPUT PARAM'
 OUTPUT_PARAM_EXTNAME = 'IMAGE-OI OUTPUT PARAM'
+RESERVED_KEYWORDS = ['XTENSION', 'BITPIX', 'NAXIS', 'NAXIS1', 'NAXIS2',
+                     'PCOUNT', 'GCOUNT', 'TFIELDS', 'EXTNAME', 'EXTVER']
+# :TODO: ordered dict
+DEFAULT_PARAM = {'WAVE_MIN': 0.1e-6, 'WAVE_MAX': 50e-6,
+                 'USE_VIS': True, 'USE_VIS2': True, 'USE_T3': True,
+                 'MAXITER': 200, 'RGL_NAME': 'mem_prior', 'RGL_WGT': 1e5}
 
 
-def parse_keyval(arg):
+def create(args):
+    """Create new image reconstruction input file."""
+    if not args.overwrite and os.path.exists(args.inputfile):
+        sys.exit("Not creating '%s' as it already exists." % args.inputfile)
+
+    # Create initial image
+    img = InitImg(INIT_IMG_EXTNAME, args.naxis1, args.naxis1)
+
+    # Create input parameters with defaults
+    inputParam = fits.Header()
+    inputParam['EXTNAME'] = INPUT_PARAM_EXTNAME
+    inputParam['INIT_IMG'] = INIT_IMG_EXTNAME  # :TODO:
+    for key in DEFAULT_PARAM.keys():
+        inputParam[key] = DEFAULT_PARAM[key]
+
+    # Overwrite user-specified parameters
+    for key, value in args.param:
+        inputParam[key] = value
+
+    # Write FITS file
+    hdulist = fits.HDUList(hdus=[img.makePrimaryHDU(),
+                                 fits.BinTableHDU(header=inputParam)])
+    hdulist.writeto(args.inputfile)
+
+
+def copyimage(args):
+    """Copy image to existing image reconstruction input/output file."""
+
+    # Open files
+    try:
+        with fits.open(args.imagefile) as sourceHduList, \
+             fits.open(args.inputfile) as destHduList:
+
+            # Find source and destination image HDUs
+            try:
+                sourceParam = sourceHduList[INPUT_PARAM_EXTNAME].header
+                sourceImageHdu = sourceHduList[sourceParam['INIT_IMG']]
+            except KeyError:
+                # Fallback to primary HDU
+                sourceImageHdu = destHduList[0]
+            destParam = destHduList[INPUT_PARAM_EXTNAME].header
+            destImageHdu = destHduList[destParam['INIT_IMG']]
+
+            # Check dimensions match
+            if sourceImageHdu.data.shape != destImageHdu.data.shape:
+                sys.exit("Image dimensions %s do not match destination %s" %
+                         (sourceImageHdu.data.shape, destImageHdu.data.shape))
+
+            # Copy image
+            destImageHdu.data = sourceImageHdu.data
+            sourceHduList.writeto(args.inputfile, clobber=True)
+
+    except IOError, msg:
+        sys.exit(msg)
+
+
+def edit(args):
+    """Edit existing image reconstruction input/output file."""
+    try:
+        with fits.open(args.inputfile) as hdulist:
+            try:
+                inputParam = hdulist[INPUT_PARAM_EXTNAME].header
+            except KeyError:
+                sys.exit("Specified file '%s' has no '%s' HDU to edit." %
+                         (args.inputfile, INPUT_PARAM_EXTNAME))
+            for key, value in args.param:
+                inputParam[key] = value
+            hdulist.writeto(args.inputfile, clobber=True)
+
+    except IOError, msg:
+        sys.exit(msg)
+
+
+def show_hdu(hdu):
+    """List parameters from hdu."""
+    print '=== %s ===' % hdu.header['EXTNAME']
+    for p in hdu.header:
+        if p not in RESERVED_KEYWORDS:
+            print '%-8s = %s' % (p, hdu.header[p])
+    print '---'
+
+
+def show(args):
+    """List parameters from image reconstruction input/output file.
+
+    """
+    try:
+        with fits.open(args.inputfile) as hdulist:
+            try:
+                show_hdu(hdulist[INPUT_PARAM_EXTNAME])
+            except KeyError:
+                print "(No '%s' HDU)" % INPUT_PARAM_EXTNAME
+            try:
+                show_hdu(hdulist[OUTPUT_PARAM_EXTNAME])
+            except KeyError:
+                print "(No '%s' HDU)" % OUTPUT_PARAM_EXTNAME
+
+    except IOError, msg:
+        sys.exit(msg)
+
+
+def parse_keyword(arg):
+    """Parse command line key-value pair."""
     key, value = arg.split('=')[:2]
     try:
         value = int(value)
@@ -22,57 +131,10 @@ def parse_keyval(arg):
         except ValueError:
             pass
     return key, value
-    
-
-def create(args):
-    """Create new image reconstruction input file."""
-    if os.path.exists(args.filename):
-        sys.exit("Not creating '%s' as it already exists." % args.filename)
 
 
-def edit(args):
-    """Edit existing image reconstruction input file."""
-    try:
-        hdulist = fits.open(args.filename)
-    except IOError, msg:
-        sys.exit(msg)
-    try:
-        #inputParam = hdulist[INPUT_PARAM_EXTNAME]
-        inputParam = hdulist[0]
-    except KeyError:
-        sys.exit("Specified file '%s' has no '%s' HDU to edit." %
-                 (args.filename, INPUT_PARAM_EXTNAME))
-    for p in args.params:
-        key, value = parse_keyval(p)
-        print '%s -> %s' % (key, value)
-        inputParam.header[key] = value
-    hdulist.writeto(args.filename, clobber=True)
-
-
-def show(args):
-    """List parameters from image reconstruction input file."""
-    try:
-        hdulist = fits.open(args.filename)
-    except IOError, msg:
-        sys.exit(msg)
-    if INPUT_PARAM_EXTNAME in hdulist:
-        inputParam = hdulist[INPUT_PARAM_EXTNAME]
-        print '=== INPUT PARAMETERS ==='
-        for p in inputParam.header:
-            print p
-    else:
-        print "(No '%s' HDU)" % INPUT_PARAM_EXTNAME
-    if OUTPUT_PARAM_EXTNAME in hdulist:
-        outputParam = hdulist[OUTPUT_PARAM_EXTNAME]
-        print '=== OUTPUT PARAMETERS ==='
-        for p in outputParam.header:
-            print p
-    else:
-        print "(No '%s' HDU)" % OUTPUT_PARAM_EXTNAME
-
-
-def main():
-    """image-oi-input.py main function."""
+def create_parser():
+    """Return new ArgumentParser instance for this script."""
 
     # Create top-level parser for command line arguments
     parser = argparse.ArgumentParser(description=
@@ -82,29 +144,51 @@ def main():
     # Create parser for the "create" command
     parser_create = subparsers.add_parser('create',
                                           help='create new imaging input file')
-    parser_create.add_argument('filename',
+    parser_create.add_argument('-o', '--overwrite', action='store_true',
+                               help='Overwrite existing file')
+    parser_create.add_argument('inputfile',
                                help='FITS file to create')
-    parser_create.add_argument('naxis1',
+    parser_create.add_argument('naxis1', type=int,
                                help='Image dimension (naxis2 == naxis1)')
+    parser_create.add_argument('cdelt1', type=float,
+                               help='Pixel size /mas (cdelt2 == cdelt1)')
+    # :TODO: add initial image type and parameters?
+    parser_create.add_argument('param', nargs='*', type=parse_keyword,
+                               help='Initial parameter e.g. MAXITER=200')
     parser_create.set_defaults(func=create)
 
+    # Create parser for the "copyimage" command
+    parser_copyimage = subparsers.add_parser('copyimage', help=
+                                             'copy initial image from file')
+    parser_copyimage.add_argument('inputfile',
+                                  help='FITS file to modify')
+    parser_copyimage.add_argument('imagefile',
+                                  help='FITS file containing initial image')
+    parser_copyimage.set_defaults(func=copyimage)
+
     # Create parser for the "edit" command
+    # :TODO: "set" better?
     parser_edit = subparsers.add_parser('edit',
                                         help='edit existing input file')
-    parser_edit.add_argument('filename',
-                             help='FITS file to edit')
-    parser_edit.add_argument('params', nargs='*',
-                             help='Replacement parameters e.g. MAXITER=200')
+    parser_edit.add_argument('inputfile',
+                             help='FITS file to modify')
+    parser_edit.add_argument('param', nargs='*', type=parse_keyword,
+                             help='Replacement parameter e.g. MAXITER=200')
     parser_edit.set_defaults(func=edit)
 
     # Create parser for the "show" command
     parser_show = subparsers.add_parser('show',
                                         help='list parameters from input file')
-    parser_show.add_argument('filename',
+    parser_show.add_argument('inputfile',
                              help='FITS file to interrogate')
     parser_show.set_defaults(func=show)
 
-    # Parse the args and call function corresponding to the user's command
+    return parser
+
+
+def main():
+    """image-oi-input.py main function."""
+    parser = create_parser()
     args = parser.parse_args()
     args.func(args)
 
---
+++ tests/test_image_oi_input.py
@@ -0,0 +1,103 @@
+import unittest
+import os
+import tempfile
+
+import numpy as np
+import astropy.io.fits as fits
+
+from InitImg import MAS_TO_RAD
+from image_oi_input import create_parser, INPUT_PARAM_EXTNAME, DEFAULT_PARAM
+
+
+class ImageOiInputTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.parser = create_parser()
+        self.tempResult = tempfile.NamedTemporaryFile(suffix='.fits',
+                                                      delete=False)
+
+    def tearDown(self):
+        self.tempResult.close()
+        os.remove(self.tempResult.name)
+
+    def create(self, inputfile, naxis1=64, cdelt1=0.25, param=[]):
+        """Create a temporary input file to read as part of a test"""
+        args = self.parser.parse_args(['create', '--overwrite', inputfile,
+                                       str(naxis1), str(cdelt1)] + param)
+        args.func(args)
+
+    def test_create_exists(self):
+        """
+        File exists and --overwrite not specified, should fail with SystemExit
+        """
+        args = self.parser.parse_args(['create', self.tempResult.name,
+                                       '128', '0.5'])
+        with self.assertRaises(SystemExit):
+            args.func(args)
+
+    def test_create(self):
+        """Test create command"""
+        naxis1 = 128
+        cdelt1 = 0.5
+        args = self.parser.parse_args(['create', '--overwrite',
+                                       self.tempResult.name,
+                                       str(naxis1), str(cdelt1), 'MAXITER=50'])
+        args.func(args)
+        with fits.open(self.tempResult.name) as hdulist:
+            param = hdulist[INPUT_PARAM_EXTNAME].header
+            imageHdu = hdulist[param['INIT_IMG']]
+            self.assertEqual(param['NAXIS'], 2)
+            self.assertEqual(imageHdu.header['NAXIS1'], naxis1)
+            self.assertEqual(imageHdu.header['NAXIS2'], naxis1)
+            #self.assertAlmostEqual(imageHdu.header['CDELT1'],
+            #                       cdelt1 * MAS_TO_RAD)
+            #self.assertAlmostEqual(imageHdu.header['CDELT2'],
+            #                       cdelt1 * MAS_TO_RAD)
+            for key in DEFAULT_PARAM.keys():
+                self.assertIsNotNone(param[key])
+            self.assertEqual(param['MAXITER'], 50)
+
+    def test_copyimage(self):
+        """Test image copy from a second imaging input file"""
+        naxis1 = 128
+        self.create(self.tempResult.name, naxis1)
+        tempImage = tempfile.NamedTemporaryFile(suffix='.fits', delete=False)
+        self.create(tempImage.name, naxis1)
+        args = self.parser.parse_args(['copyimage', self.tempResult.name,
+                                       tempImage.name])
+        args.func(args)
+        tempImage.close()
+        os.remove(tempImage.name)
+
+    def test_copyimage_primary(self):
+        """Test image copy from primary HDU (fallback behaviour)"""
+        naxis1 = 128
+        self.create(self.tempResult.name, naxis1)
+        tempImage = tempfile.NamedTemporaryFile(suffix='.fits', delete=False)
+        fits.PrimaryHDU(data=np.zeros((naxis1, naxis1))).writeto(tempImage)
+        args = self.parser.parse_args(['copyimage', self.tempResult.name,
+                                       tempImage.name])
+        args.func(args)
+        tempImage.close()
+        os.remove(tempImage.name)
+
+    def test_edit(self):
+        """Test edit command"""
+        self.create(self.tempResult.name, param=['MYKEY1=99.12'])
+        args = self.parser.parse_args(['edit', self.tempResult.name,
+                                       'MYKEY1=0.467', 'MYKEY2=foo'])
+        args.func(args)
+        with fits.open(self.tempResult.name) as hdulist:
+            param = hdulist[INPUT_PARAM_EXTNAME].header
+            self.assertAlmostEqual(param['MYKEY1'], 0.467)
+            self.assertEqual(param['MYKEY2'], 'foo')
+
+    def test_show(self):
+        """Test show command"""
+        self.create(self.tempResult.name)
+        args = self.parser.parse_args(['show', self.tempResult.name])
+        args.func(args)
+
+
+if __name__ == '__main__':
+    unittest.main()
