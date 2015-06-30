f7615601680d84f26a86a14a4421afc665287150
John Young <jsy1001@cam.ac.uk>
2015-06-30 10:37:08 +0100

    Added test of code formatting.

---
+++ tests/test_pep8.py
@@ -0,0 +1,17 @@
+import unittest
+import pep8
+
+
+class TestCodeFormat(unittest.TestCase):
+
+    def test_pep8_conformance(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['image_oi_input.py', 'GreyImg.py',
+                                        'HDUListPlus.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+
+if __name__ == '__main__':
+    unittest.main()
